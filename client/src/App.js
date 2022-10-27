3c7815e5d0278a288482467f1788fe8bf93e2b57
Saksham <saksham7822pandey@gmail.com>
2022-10-27 19:44:12 +0530

    adds signin and sign up

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
+import Cards from './components/Cards';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Signup from './components/Signup';
+import Signin from './components/Signin';
+import {
+  Route,
+  Routes
+} from "react-router-dom";
 function App() {
   return (
     <div>
       <Navbar/>
+      <Routes>
+        <Route path="/" element={<Cards/>}/>
+      <Route  path="/signin" element = {<Signin/>}/>
+      <Route  path="/signup" element = {<Signup/>}/>
+      </Routes>
       <Footer/>
     </div>
-  );
+  )
 }
 
 export default App;
